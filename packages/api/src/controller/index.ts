e0ca2b6f63bc2476014d027e8a0630146f21c8a6
leoweyr <contact@leoweyr.com>
2025-03-12 16:40:53 +0800

    feat(api): standardize success responses using unified interface

--- packages/api/src/controller/index.ts
+++ packages/api/src/controller/index.ts
@@ -1,2 +1,4 @@
+export { Router, Request, Response, NextFunction } from "express";
+
 export { ErrorResponseType } from "./enums/ErrorResponseType";
 export { ErrorResponseCapable } from "./ErrorResponseCapable";
